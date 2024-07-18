51d32a00a807c2337183cb73d6d43475262fa312
eshenka <yuliaolegyureva@gmail.com>
2024-07-18 21:00:23 +0700

    trying color the screen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use crossterm::style;
+use crossterm::terminal;
+use crossterm::{QueueableCommand, queue, execute};
+use std::io::{Write, stdout};
+
 fn main() {
-    println!("Hello, world!");
+    let mut stdout = stdout();
+
+    execute!(stdout, terminal::EnterAlternateScreen).unwrap();
+    queue!(stdout, style::SetBackgroundColor(style::Color::Blue));
+
+    stdout.flush().unwrap();
 }

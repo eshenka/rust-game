ab8409ef76e68ec651353a4cfcd205e636842a23
eshenka <yuliaolegyureva@gmail.com>
2024-07-18 22:15:43 +0700

    messing with cursor

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
 use crossterm::style;
+use crossterm::style::Stylize;
 use crossterm::terminal;
-use crossterm::{QueueableCommand, queue, execute};
+use crossterm::cursor;
+use crossterm::{QueueableCommand, queue, ExecutableCommand, execute};
 use std::io::{Write, stdout};
 
 fn main() {
     let mut stdout = stdout();
 
+    stdout.execute(terminal::Clear(terminal::ClearType::All)).unwrap();
+
     execute!(stdout, terminal::EnterAlternateScreen).unwrap();
-    queue!(stdout, style::SetBackgroundColor(style::Color::Blue));
+    queue!(stdout, style::SetBackgroundColor(style::Color::Blue), style::SetForegroundColor(style::Color::Red));
+
+    stdout.flush().unwrap();
+
+    for x in 0..150 {
+        for y in 0..40 {
+            if x == 0 || y == 0 || x == 149 || y == 39 {
+                queue!(stdout, cursor::MoveTo(x, y), style::PrintStyledContent("0".blue())).unwrap();
+            }
+        }
+    }
+
+    queue!(stdout, cursor::MoveTo(60, 10));
 
     stdout.flush().unwrap();
 }
+
